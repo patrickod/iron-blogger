757a860ce9a67f9a7ba43e6c34248788a387a3cf
Benjamin Gleitzman <gleitz@mit.edu>
2013-10-20 22:43:40 -0700

    additions for sending email

--- weekly-update.py
+++ weekly-update.py
@@ -1,91 +1,124 @@
 #!/usr/bin/python
-import render
-import os
 import sys
 import xmlrpclib
 import subprocess
-import datetime
 import yaml
+import smtplib
 
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+import render
 from config import *
 
-dry_run = False
-
-args = sys.argv[1:]
-if args[0] == '-n':
-    dry_run = True
-    args = args[1:]
-
-date = args[0]
-
-with open('ledger', 'a') as f:
-    f.write("\n")
-    f.write(render.render_template('templates/ledger', date))
-
-if not dry_run:
-    subprocess.check_call(["git", "commit", "ledger",
-                           "-m", "Update for %s" % (date,)])
-
-debts = render.get_debts()
-punt = []
-
-with open('ledger', 'a') as f:
-    f.write("\n")
-    for (user, debt) in debts:
-        if debt <= (FINE_SIZE * 6): continue
-        punt.append(user)
-        f.write("""\
-%(date)s Punt
-  Pool:Owed:%(user)s  -%(debt)s
-  User:%(user)s
-""" % {'user': user, 'debt': debt, 'date': date})
-
-
-if not dry_run:
-    text = render.render_template('templates/week.tmpl', date, punt=punt)
-
-    lines = text.split("\n")
-    title = lines[0]
-    body  = "\n".join(lines[1:])
-
-    page = dict(title = title, description = body)
-
+def send_email(address, html):
     with open('secret.txt', 'r') as f:
-        passwd  = f.read().strip()
+        lines = f.read().strip()
+        username, password = lines.split('||')
 
-    x = xmlrpclib.ServerProxy(XMLRPC_ENDPOINT)
-    x.metaWeblog.newPost(BLOG_ID, USER, passwd, page, True)
+    print 'Sending weekly email to {0}...'.format(address)
+    smtp = smtplib.SMTP('smtp.gmail.com', 587)
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.ehlo()
+    smtp.login(username, password)
+    msg = MIMEMultipart('mixed')
+    msg['Subject'] = 'Weekly Recap'
+    msg['To'] = address
+    msg['From'] = username
+    # msg.attach(MIMEText('Boarding pass is attached', 'plain'))
+    msg_bp = MIMEText(html, 'html')
+    msg_bp.add_header('content-disposition', 'attachment', filename='boarding_pass.html')
+    msg.attach(msg_bp)
+    smtp.sendmail(address, username, msg.as_string())
 
-email = render.render_template('templates/email.html', date, punt=punt)
+    print 'Boarding pass sent successfully'
+    smtp.close()
 
-if dry_run:
-    print email
-else:
-    #TODO(gleitz): send email here
-    # p = subprocess.Popen(['mutt', '-H', '/dev/stdin'],
-    #                      stdin=subprocess.PIPE)
-    # p.communicate(email)
-    print email
 
-with open('out/email.txt', 'w') as f:
-    f.write(email)
+def generate_email():
+    dry_run = False
 
-if punt:
-    with open('bloggers.yml') as b:
-        bloggers = yaml.safe_load(b)
-    for p in punt:
-        if bloggers.get(p) and 'end' not in bloggers[p]:
-            bloggers[p]['end'] = date
-    with open('bloggers.yml','w') as b:
-        yaml.safe_dump(bloggers, b)
+    args = sys.argv[1:]
+    if args[0] == '-n':
+        dry_run = True
+        args = args[1:]
+
+    date = args[0]
+
+    with open('ledger', 'a') as f:
+        f.write("\n")
+        f.write(render.render_template('templates/ledger', date))
 
     if not dry_run:
-        subprocess.check_call(["git", "commit", "ledger", "bloggers.yml",
-                               "-m", "Punts for %s" % (date,)])
+        subprocess.check_call(["git", "commit", "ledger",
+                               "-m", "Update for %s" % (date,)])
 
-# if it's a dry run, lets set the ledger back to the beginning state
-if dry_run:
-    subprocess.check_call(["git", "checkout", "ledger"])
+    debts = render.get_debts()
+    punt = []
+
+    with open('ledger', 'a') as f:
+        f.write("\n")
+        for (user, debt) in debts:
+            if debt <= (FINE_SIZE * 6):
+                continue
+            punt.append(user)
+            f.write("""\
+    %(date)s Punt
+      Pool:Owed:%(user)s  -%(debt)s
+      User:%(user)s
+    """ % {'user': user, 'debt': debt, 'date': date})
+
+
+    if not dry_run:
+        text = render.render_template('templates/week.tmpl', date, punt=punt)
+
+        lines = text.split("\n")
+        title = lines[0]
+        body  = "\n".join(lines[1:])
+
+        page = dict(title = title, description = body)
+
+        with open('secret.txt', 'r') as f:
+            passwd  = f.read().strip()
+
+        x = xmlrpclib.ServerProxy(XMLRPC_ENDPOINT)
+        x.metaWeblog.newPost(BLOG_ID, USER, passwd, page, True)
+
+    email = render.render_template('templates/email.html', date, punt=punt)
+
+    if dry_run:
+        print email
+        send_email('gleitz@mit.edu', email)
+    else:
+        #TODO(gleitz): send email here
+        # p = subprocess.Popen(['mutt', '-H', '/dev/stdin'],
+        #                      stdin=subprocess.PIPE)
+        # p.communicate(email)
+        print email
+
+    with open('out/email.txt', 'w') as f:
+        f.write(email)
 
     if punt:
-        subprocess.check_call(["git", "checkout", "bloggers.yml"])
+        with open('bloggers.yml') as b:
+            bloggers = yaml.safe_load(b)
+        for p in punt:
+            if bloggers.get(p) and 'end' not in bloggers[p]:
+                bloggers[p]['end'] = date
+        with open('bloggers.yml','w') as b:
+            yaml.safe_dump(bloggers, b)
+
+        if not dry_run:
+            subprocess.check_call(["git", "commit", "ledger", "bloggers.yml",
+                                   "-m", "Punts for %s" % (date,)])
+
+    # if it's a dry run, lets set the ledger back to the beginning state
+    if dry_run:
+        subprocess.check_call(["git", "checkout", "ledger"])
+
+        if punt:
+            subprocess.check_call(["git", "checkout", "bloggers.yml"])
+
+if __name__ == '__main__':
+    generate_email()

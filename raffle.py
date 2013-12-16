408513b68f4dba55e795bb2aeb4a599603cc2968
Benjamin Gleitzman <gleitz@mit.edu>
2013-12-16 09:25:21 -0800

    Update for raffle

--- raffle.py
+++ raffle.py
@@ -2,38 +2,38 @@ import random
 import sys
 import yaml
 
+from collections import Counter
 from dateutil.parser import parse
 
-import render
-
-from config import START
-
-
-def raffle(last_meetup):
+def shuffle(last_meetup_datetime):
     with open('out/report.yml') as r:
         report = yaml.safe_load(r)
     with open('bloggers.yml') as f:
         users = yaml.safe_load(f)
 
-    last_meetup_datetime = parse(last_meetup)
-
-    tickets = []
-    for (un, rec) in users.items():
-        if rec.get('end'):
+    tickets = Counter()
+    for (username, info) in users.items():
+        if info.get('end'):
             continue
-        weeks = report.get(un, [])
+        weeks = report.get(username, [])
         for week in weeks:
             if not week or not week[0]:
                 continue
             if week[0]['date'] > last_meetup_datetime:
-                tickets.append(un)
-    print tickets
-    return random.choice(tickets)
+                tickets[username] += 1
+    return tickets
 
+def raffle(last_meetup_str):
+    last_meetup_datetime = parse(last_meetup_str)
+    tickets = shuffle(last_meetup_datetime)
+    print "Raffle contains the following tickets:"
+    print tickets
+    print "And the winner is:"
+    return random.choice(list(tickets.elements()))
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
+    if len(sys.argv) == 2:
         date = sys.argv[1]
         print raffle(date)
     else:
-        print "Must pass date %Y-%m-%d"
+        print "Usage: python raffle.py <LAST_MEETUP_DATE as %Y-%m-%d>"
